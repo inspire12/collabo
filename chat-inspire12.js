03e945e94a3ed1f4a9e5f57437a0881c25c6a871
inspire12 <ox4443@naver.com>
2016-02-20 11:58:52 +0900

    fdsa

--- chat-inspire12.js
+++ chat-inspire12.js
@@ -1,9 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
 
-  res.send('<h1>Hello world</h1>');
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(3000, function(){
